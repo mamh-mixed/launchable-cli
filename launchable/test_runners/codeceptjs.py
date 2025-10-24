cf9bb3c2e83d737c0b25957966ff331e97398a90
Naoto Ono <nono@cloudbees.com>
2025-10-24 17:26:18 +0900

    Support CodeceptJS profile

---
+++ launchable/test_runners/codeceptjs.py
@@ -0,0 +1,28 @@
+import json
+from typing import List
+
+import click
+
+from ..testpath import TestPath
+from . import launchable
+
+
+@launchable.subset
+def subset(client):
+    def handler(output: List[TestPath], rests: List[TestPath]):
+        if client.rest:
+            with open(client.rest, "w+", encoding="utf-8") as f:
+                f.write(json.dumps({"tests": [client.formatter(t) for t in rests]}))
+        if output:
+            click.echo(json.dumps({"tests": [client.formatter(t) for t in output]}))
+
+    # read lines as test file names
+    for t in client.stdin():
+        client.test_path(t.rstrip("\n"))
+    client.output_handler = handler
+
+    client.run()
+
+
+record_tests = launchable.CommonRecordTestImpls(__name__).report_files()
+split_subset = launchable.CommonSplitSubsetImpls(__name__).split_subset()
