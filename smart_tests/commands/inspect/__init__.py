8252cb5713afe309a9124d9d6af7936a38d9bb90
Kohsuke Kawaguchi <kk@kohsuke.org>
2025-10-30 18:22:16 -0700

    Removed the 'inspect tests' command
    Yabuki-san told me we removed this command since this functionality has
    been since then replaced by the webapp

--- smart_tests/commands/inspect/__init__.py
+++ smart_tests/commands/inspect/__init__.py
@@ -2,7 +2,6 @@ from ... import args4p
 from ...app import Application
 from .model import model
 from .subset import subset
-from .tests import tests
 
 
 @args4p.group(help="Inspect test and subset data")
@@ -12,4 +11,3 @@ def inspect(app: Application):
 
 inspect.add_command(model)
 inspect.add_command(subset)
-inspect.add_command(tests)
