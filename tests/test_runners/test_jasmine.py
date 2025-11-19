e5acb00a9fc8f02b433ca27981f752160e2fbfca
gayanW <gweerakutti@cloudbees.com>
2025-11-19 11:06:09 +0900

    Add test test_jasmine.py#test_record_tests_without_filename

--- tests/test_runners/test_jasmine.py
+++ tests/test_runners/test_jasmine.py
@@ -11,12 +11,25 @@ class JasmineTest(CliTestCase):
     @responses.activate
     @mock.patch.dict(os.environ,
                      {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
-    def test_record_test_json(self):
+    def test_record_tests_json(self):
         result = self.cli('record', 'tests', '--session', self.session,
                           'jasmine', str(self.test_files_dir.joinpath("jasmine-test-results.json")))
 
         self.assert_success(result)
         self.assert_record_tests_payload('record_test_result.json')
+
+    @responses.activate
+    @mock.patch.dict(os.environ,
+                     {"LAUNCHABLE_TOKEN": CliTestCase.launchable_token})
+    def test_record_tests_without_filename(self):
+        result = self.cli('record', 'tests', '--session', self.session,
+                          'jasmine', str(self.test_files_dir.joinpath("jasmine-test-results-v3.99.0.json")))
+
+        self.assertIn(
+            "does not appear to be valid format. "
+            "Make sure you are using Jasmine >= v4.6.0 and jasmine-json-test-reporter as the reporter.",
+            result.output
+        )
 
     @responses.activate
     @mock.patch.dict(os.environ,
