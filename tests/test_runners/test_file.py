f71a18159ebc920df7a6be0e95024f469352eb0e
Naoto Ono <nono@cloudbees.com>
2026-02-25 14:05:38 +0900

    Use SMART_TESTS_TOKEN

--- tests/test_runners/test_file.py
+++ tests/test_runners/test_file.py
@@ -8,7 +8,7 @@ from tests.cli_test_case import CliTestCase
 
 class FileTest(CliTestCase):
     @responses.activate
-    @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.smart_tests_token})
+    @mock.patch.dict(os.environ, {"SMART_TESTS_TOKEN": CliTestCase.smart_tests_token})
     def test_record_test_file(self):
         result = self.cli(
             "record",
@@ -23,7 +23,7 @@ class FileTest(CliTestCase):
         self.assert_record_tests_payload("record_test_result.json")
 
     @responses.activate
-    @mock.patch.dict(os.environ, {"LAUNCHABLE_TOKEN": CliTestCase.smart_tests_token})
+    @mock.patch.dict(os.environ, {"SMART_TESTS_TOKEN": CliTestCase.smart_tests_token})
     def test_record_test_file_with_custom_file_path_attribute(self):
         """Test that --file-path-attribute option allows specifying custom attributes"""
         result = self.cli(
