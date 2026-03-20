5657d20226b5311eaf4485d12e31528e5c9d5848
Kohsuke Kawaguchi <kk@kohsuke.org>
2026-03-19 18:37:33 -0700

    Use GHA to check out the docs

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 import subprocess
 import tempfile
@@ -6,6 +7,8 @@ from pathlib import Path
 from setuptools import setup
 from setuptools.command.build_py import build_py
 
+_DOCSITE_REPO = 'git@github.com:cloudbees/docsite-cloudbees-smart-tests.git'
+
 
 class BuildPyWithDocs(build_py):
     def run(self):
@@ -13,13 +16,18 @@ class BuildPyWithDocs(build_py):
         if docs_dst.exists():
             shutil.rmtree(docs_dst)
 
-        with tempfile.TemporaryDirectory() as tmpdir:
-            subprocess.run(
-                ['git', 'clone', '--depth=1',
-                 'git@github.com:cloudbees/docsite-cloudbees-smart-tests.git', tmpdir],
-                check=True,
-            )
-            shutil.copytree(Path(tmpdir) / 'docs', docs_dst)
+        # In CI, actions/checkout pre-checks out the docsite and sets this variable
+        # so we never need to handle auth here. Fall back to git clone for local dev.
+        precheck = os.environ.get('DOCSITE_DOCS_PATH')
+        if precheck:
+            shutil.copytree(Path(precheck), docs_dst)
+        else:
+            with tempfile.TemporaryDirectory() as tmpdir:
+                subprocess.run(
+                    ['git', 'clone', '--depth=1', _DOCSITE_REPO, tmpdir],
+                    check=True,
+                )
+                shutil.copytree(Path(tmpdir) / 'docs', docs_dst)
 
         super().run()
 
